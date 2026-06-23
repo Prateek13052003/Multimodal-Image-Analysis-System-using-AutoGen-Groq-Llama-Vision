53ad62fbd5c520a6e8f7fbf1450895c7d8ecc4e7
Prateek Choudhary <prateekchoudhary@Prateeks-MacBook-Air.local>
2026-06-24 01:40:33 +0530

    fix vercel cors

--- app/main.py
+++ app/main.py
@@ -12,7 +12,10 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://multimodal-image-analysis-system-us.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
